b4e7ebf4f5653efd0e9c7210b9bc20114bb6a598
sam-price <samprice.ethos@gmail.com>
2017-01-20 12:48:38 +0000

    Added css files, changed to scss in some cases. Added styles and made sure application.js and application.scss has all imports necessary

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require jquerty.easing.min
+//= require jasny-bootstrap
+//= require supersized.3.2.7
 //= require_tree .
+
